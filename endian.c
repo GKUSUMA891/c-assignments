36fce6833b1547e9cd535eedbd3bce430c6b940b
kusuma <kusuma.g@thundersoft.com>
2022-04-26 23:49:09 -0700

    little endian r big endian

---
+++ endian.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int x=3012305879;
+   char b=(char) x;
+
+    if(b=30)
+        printf("big endian \n");
+    else
+        printf("little endian");
+
+    return 0;
+}
+
