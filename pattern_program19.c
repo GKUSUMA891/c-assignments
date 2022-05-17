d6e2642d60c6e26dab734981967ce9e65d70d78b
GKUSUMA891 <98938017+GKUSUMA891@users.noreply.github.com>
2022-05-17 20:09:08 +0530

    pattern_program

---
+++ pattern_program19.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n=6,k=0;
+    for(i=1;i<=n;i++)
+    {
+        if(i%2 != 0){
+               k++;
+            }
+        for(j=1;j<=i;j++)
+        {
+            if(i%2 != 0){
+                printf("%d",k);
+                printf(" ");
+            }
+            else{
+                printf("*");
+                printf(" ");
+            }
+        }
+        printf("\n");
+
+    }
+}
